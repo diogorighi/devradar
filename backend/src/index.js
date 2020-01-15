2de7a0cc923813c9a08185fc20d140258bbf74b9
Diogo Righi <diogorighi@gmail.com>
2020-01-15 20:09:44 -0300

    Aula #3

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const routes = require("./routes");
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect(MONGODB_URI, {
   useCreateIndex: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
