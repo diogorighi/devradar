31373a55c940a1e57baced0d60348d9a26406909
Diogo Righi <diogorighi@gmail.com>
2020-01-17 13:28:36 -0300

    Aula #5

--- backend/src/controllers/DevController.js
+++ backend/src/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require("axios");
 const Dev = require("../models/Dev");
 const convertStringToArray = require("../utils/convertStringToArray");
+const { findConnections, sendMessage } = require("../websocket");
 
 module.exports = {
   async index(request, response) {
@@ -32,6 +33,10 @@ module.exports = {
         techs: techsArray,
         location
       });
+
+      const sendSocketMessageTo = findConnections({ latitude, longitude }, techsArray);
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
     }
 
     return response.json(dev);
--- backend/src/index.js
+++ backend/src/index.js
@@ -2,9 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const http = require("http");
 const routes = require("./routes");
+const { setupWebSocket } = require("./websocket");
 
 const app = express();
+const server = http.Server(app);
+
+setupWebSocket(server);
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
@@ -20,6 +25,6 @@ app.use(routes);
 
 const PORT = process.env.PORT;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}!`);
 });
